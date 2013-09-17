eb7fc76712edf19c4c55edf4559a65b1cc2e6716
Alex Savin <karismafilms@gmail.com>
2013-09-17 16:23:18 +0300

    Drawing vertical segment implementation

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ require './pixeleditor'
 
 # Initializing values
 @input = []
-@exit_commands = ['x', 'X', 'exit']
+@exit_commands = ['x', 'X', 'exit', 'EXIT']
 @the_image = []
 
 # Initialize the Editor
@@ -18,32 +18,47 @@ while (!@exit_commands.include?(@input[0]))
   @input = ask("Editor (space sep arguments) <%= color('>', GREEN) %>  ", lambda { |str| str.split(/ \s*/) })
   
   # Check what kind of command is entered
-  case @input[0]
+  case @input[0].upcase
   when 'I'
     # Create new image
-    break
+    @the_image = @editor.create_new_image(@input[1].to_i, @input[2].to_i)
+    
   when 'C'
     # Clear the image, set all pixels to 'O'
-    break
+    @the_image = @editor.clear_image(@the_image)
+    
   when 'L'
     # Colour one pixel with given colour
-    break
+    @the_image = @editor.colour_one_pixel(@the_image, @input[1].to_i, @input[2].to_i, @input[3])
+    
   when 'V'
     # Draw a vertical segment
-    break
+    @the_image = @editor.draw_vertical_segment(@the_image, @input[1].to_i, @input[2].to_i, @input[3].to_i, @input[4])
+    
   when 'H'
     # Draw a horizontal segment
-    break
+    
   when 'F'
     # Fill the region
-    break
+    
   when 'S'
     # Print out the current image
+    @editor.printout(@the_image)
+    
+  when *@exit_commands
     break
-  when 'X'
-    break
+  
+  when 'HELP'
+    puts "\nAvailable commands"
+    puts "I M N --- Create a new M x N image with all pixels coloured white (O)"
+    puts "C --- Clears the table, setting all pixels to white (O)"
+    puts "L X Y C --- Colours the pixel (X,Y) with colour C"
+    puts "V X Y1 Y2 C --- Draw a vertical segment of colour C in column X between rows Y1 and Y2 (inclusive)"
+    puts "H X1 X2 Y C --- Draw a horizontal segment of colour C in row Y between columns X1 and X2 (inclusive)"
+    puts "X --- exit"
+    puts "\n\n"
   else
-    puts 'Unknown command :('
+    puts 'Unknown command :( Try \'help\''
   end
 end
 
--- pixeleditor.rb
+++ pixeleditor.rb
@@ -2,4 +2,27 @@ class PixelEditor
   def create_new_image(m, n)
     Array.new(n) { Array.new(m) {'O'} }
   end
+  
+  # Expected input:
+  # Array of arrays of strings representing pixels
+  # All pixels are reset to 'O' value
+  def clear_image(pixels)
+    pixels.map! { |row| row.map { |p| p = 'O'}}
+  end
+  
+  def printout(pixels)
+    pixels.each do |row|
+      puts row.join
+    end
+  end
+  
+  def colour_one_pixel(pixels, x, y, colour)
+    pixels[x][y] = colour
+    pixels
+  end
+  
+  def draw_vertical_segment(pixels, x, y1, y2, colour)
+    pixels[y1..y2].map! { |row| row[x] = colour }
+    pixels
+  end
 end
